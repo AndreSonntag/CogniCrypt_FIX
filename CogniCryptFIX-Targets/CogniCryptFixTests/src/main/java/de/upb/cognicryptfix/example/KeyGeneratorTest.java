24e145dcc9251b66abc83a985cfc09258e29598b
AndreSonntag <Andre-Sonntag@gmx.de>
2019-10-28 14:32:02 +0100

    Add new test, update old test
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

---
+++ CogniCryptFIX-Targets/CogniCryptFixTests/src/main/java/de/upb/cognicryptfix/example/KeyGeneratorTest.java
@@ -0,0 +1,17 @@
+package de.upb.cognicryptfix.example;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+
+public class KeyGeneratorTest {
+	
+	public void gen() throws NoSuchAlgorithmException {
+		
+		KeyGenerator kg = KeyGenerator.getInstance("AES");
+		kg.init(100);
+		kg.generateKey();
+		
+	}
+
+}
--- CogniCryptFIX-Targets/CogniCryptFixTests/src/main/java/de/upb/cognicryptfix/example/PBEKeySpecTest.java
+++ CogniCryptFIX-Targets/CogniCryptFixTests/src/main/java/de/upb/cognicryptfix/example/PBEKeySpecTest.java
@@ -9,10 +9,18 @@ public class PBEKeySpecTest {
 	//static salt
 	byte[] salt = { 15, -12, 94, 0, 12, 3, -65, 73, -1, -84, -35 };
 
-	private void generateKey(String password) throws NoSuchAlgorithmException, GeneralSecurityException {
+	private void generateKey() throws NoSuchAlgorithmException, GeneralSecurityException {
+		
+		//password should never stored as String
+		String passwordStr = "testtesttest";	
+		passwordStr = "HalloWelt";
+		
+		
+		//char[] passwordCharArry = {'a','b','c'};
+		
 		//too low iterations
 		int iteration = 9999;
-		PBEKeySpec pBEKeySpec = new PBEKeySpec(password.toCharArray(), salt, iteration, 256);
+		PBEKeySpec pBEKeySpec = new PBEKeySpec(passwordStr.toCharArray(), salt, iteration, 256);
 	}
 
 }
