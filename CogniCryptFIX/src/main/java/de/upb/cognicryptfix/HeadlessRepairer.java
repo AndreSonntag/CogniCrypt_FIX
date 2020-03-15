3c778b508c7a86363b7f2f72558e509332183db7
AndreSonntag <Andre-Sonntag@gmx.de>
2020-03-15 13:31:44 +0100

    minor
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

--- CogniCryptFIX/src/main/java/de/upb/cognicryptfix/HeadlessRepairer.java
+++ CogniCryptFIX/src/main/java/de/upb/cognicryptfix/HeadlessRepairer.java
@@ -1,5 +1,7 @@
 package de.upb.cognicryptfix;
 
+import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -9,9 +11,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
+import crypto.rules.CrySLRule;
 import de.upb.cognicryptfix.analysis.CryptoAnalysis;
 import de.upb.cognicryptfix.analysis.CryptoAnalysisListener;
-import de.upb.cognicryptfix.utils.CrySLReaderUtils;
+import de.upb.cognicryptfix.crysl.CrySLEntityPool;
+import de.upb.cognicryptfix.crysl.CrySLReaderUtils;
 import de.upb.cognicryptfix.utils.MavenProject;
 import de.upb.cognicryptfix.utils.Utils;
 
@@ -23,6 +27,7 @@ public class HeadlessRepairer {
 
 	private static final Logger logger = LogManager.getLogger(HeadlessRepairer.class.getSimpleName());
  	private static CommandLine options;
+ 	private static List<CrySLRule> rules;
 
 	public static void main(String... args) throws ParseException {
 		infoLogLevel();
@@ -35,7 +40,9 @@ public class HeadlessRepairer {
 		final String applicationClassPath = options.hasOption("applicationCp") ? options.getOptionValue("applicationCp") : "applicationClassPath";
 
 		MavenProject analysingProject = Utils.createAndCompile(applicationClassPath);
-		CryptoAnalysis analysis = new CryptoAnalysis();
+		rules = readRules();
+		
+		CryptoAnalysis analysis = new CryptoAnalysis(rules);
 		CryptoAnalysisListener listener = new CryptoAnalysisListener();
 		analysis.runSoot(analysingProject, listener);
 		
@@ -47,6 +54,13 @@ public class HeadlessRepairer {
 //		analysis.runSoot(analysingProject, listener);
 //	}
 	
+	public static List<CrySLRule> getCrySLRules() {
+		return rules;
+	}
+	
+	private static List<CrySLRule> readRules(){
+		return CrySLReaderUtils.readRulesFromSourceFiles(Constants.crySLRulePath);
+	}
 	
 	public static void infoLogLevel() {
 		setLoggerLevel(Level.INFO);	
