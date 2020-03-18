f4d7f4582c69385de8ccbbf54147b289e4577eb8
AndreSonntag <Andre-Sonntag@gmx.de>
2020-03-18 13:57:03 +0100

    Add NoEnsuredPredicateException
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

---
+++ CogniCryptFIX/src/main/java/de/upb/cognicryptfix/exception/NoEnsuredPredicateException.java
@@ -0,0 +1,13 @@
+package de.upb.cognicryptfix.exception;
+
+public class NoEnsuredPredicateException extends Exception {
+	
+	public NoEnsuredPredicateException(String errorMessage) {
+        super(errorMessage);
+    }
+	
+	public NoEnsuredPredicateException() {
+        super();
+    }
+
+}
