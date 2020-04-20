0f5f84985fd0b395d1efe831d4c6c898bbce33ad
AndreSonntag <Andre-Sonntag@gmx.de>
2020-04-20 11:53:51 +0200

    add Utils to target project
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

---
+++ CogniCryptFIX-Targets/CogniCryptFixTests/src/main/java/de/upb/cognicryptfix/test/utils/Utils.java
@@ -0,0 +1,60 @@
+package de.upb.cognicryptfix.test.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+
+public class Utils {
+	
+	public Utils() {
+		
+	}
+	
+	public char[] readFileContent(String path){
+		char[] content = new char[100];
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			for(int i = 0, charNum = br.read(); charNum != -1; i++) {
+				content[i] = (char) charNum;
+				charNum = br.read();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return content;
+	}
+
+	public KeyStore createKeyStore() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+		KeyStore ks = KeyStore.getInstance("jceks");
+		ks.load(null, readFileContent("pathToPassword"));
+		return ks;
+	}
+	
+	public KeyManager[] getKeyManagers(KeyStore store) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+		KeyManager[] manager = null;
+		KeyManagerFactory factory = KeyManagerFactory.getInstance("PKIX");
+		factory.init(store, readFileContent("pathToPasswordFile"));
+		manager = factory.getKeyManagers();
+		return manager;
+	}
+	
+	public TrustManager[] getTrustManager(KeyStore store) throws NoSuchAlgorithmException, KeyStoreException {
+		TrustManager[] managers = null;
+		TrustManagerFactory factory = TrustManagerFactory.getInstance("PKIX");
+		factory.init(store);
+		managers = factory.getTrustManagers();
+		return managers;
+	}
+}
